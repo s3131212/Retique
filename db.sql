7f6da75fa77b95e712eef276f41ed1123f9c84c3
Allen Chou <s3131212@gmail.com>
2019-05-21 19:28:23 +0800

    Add default to db.sql

--- db.sql
+++ db.sql
@@ -1,17 +1,13 @@
-DROP TABLE IF EXISTS `questions`;
-
 CREATE TABLE `questions` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `roomid` varchar(32) NOT NULL DEFAULT '',
+  `roomid` varchar(32) NOT NULL DEFAULT '0',
   `content` varchar(1024) DEFAULT NULL,
   `author` varchar(32) DEFAULT NULL,
-  `replyid` int(11) DEFAULT NULL,
-  `like` int(11) NOT NULL,
+  `replyid` int(11) DEFAULT '0',
+  `like` int(11) NOT NULL DEFAULT '0',
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-DROP TABLE IF EXISTS `rooms`;
 
 CREATE TABLE `rooms` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
